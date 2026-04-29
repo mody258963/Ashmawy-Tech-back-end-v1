d1e211c92f7b80848503ed6592723f2078ce9629
Mohamed Ashmawy <122531523+mody258963@users.noreply.github.com>
2026-04-29 15:41:02 +0300

    update

--- Ashmawy-tech/resources/views/layouts/guest.blade.php
+++ Ashmawy-tech/resources/views/layouts/guest.blade.php
@@ -11,13 +11,24 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @php
+            $hasViteManifest = file_exists(public_path('build/manifest.json'));
+        @endphp
+
+        @if ($hasViteManifest)
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @else
+            <link rel="stylesheet" href="{{ secure_asset('dist/css/adminlte.min.css') }}">
+            <link rel="stylesheet" href="{{ secure_asset('plugins/fontawesome-free/css/all.min.css') }}">
+            <style>
+                body { background: #f4f6f9; }
+            </style>
+        @endif
     </head>
     <body class="font-sans text-gray-900 antialiased {{ app()->getLocale() === 'ar' ? 'rtl-mode' : 'ltr-mode' }}">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
-                <a href="/">
+                <a href="{{ url('/') }}">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>
